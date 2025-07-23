aa3f226ae0ed2f40a8207bd984f1f6368b7db4c9
toyman640 <falakoadebayor@gmail.com>
2025-07-23 14:32:56 +0100

    Add app

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/my-portfolio/',  // <-- This must match the repo name
   plugins: [react()],
-  base: '/portfolio-rebuild/'
 })
