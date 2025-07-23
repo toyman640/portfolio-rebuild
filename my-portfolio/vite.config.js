79b60fcd420260925df964fd5cf77b1699e881ef
toyman640 <falakoadebayor@gmail.com>
2025-07-23 16:19:11 +0100

    check

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio/',  // <-- This must match the repo name
+  base: '/my-portfolio',  // <-- This must match the repo name
   plugins: [react()],
 })
