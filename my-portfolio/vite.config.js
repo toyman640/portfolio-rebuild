024e05848e8cab423d8f4205ed526b85aba9f4d9
toyman640 <falakoadebayor@gmail.com>
2026-07-30 14:06:45 +0100

    Fix base url

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-rebuild/my-portfolio',  // <-- This must match the repo name
+  base: '/portfolio-rebuild/',  // <-- This must match the repo name
   plugins: [react()],
 })
