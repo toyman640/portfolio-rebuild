6876f179803f46fb5f3c8030e54794118cd125dc
toyman640 <falakoadebayor@gmail.com>
2026-07-30 15:19:01 +0100

    Lets see

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-rebuild/',  // <-- This must match the repo name on git
+  base: '/portfolio-rebuild/',
   plugins: [react()],
 })
